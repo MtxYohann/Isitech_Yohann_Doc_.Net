c75e5c1a703450580ced444aae6dd9b86933c5f9
MtxYohann <137070655+Yohann-mtx@users.noreply.github.com>
2024-12-15 21:49:53 +0100

    Add ReadME

--- Projects/mvcTemplate/Program.cs
+++ Projects/mvcTemplate/Program.cs
@@ -3,16 +3,12 @@ using Microsoft.AspNetCore.Identity;
 using mvc.Data;
 using mvc.Models;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddRazorPages();
-
-
-
-
 
 var serverVersion = new MySqlServerVersion(new Version(11, 5, 2));
 builder.Services.AddDbContext<ApplicationDbContext>(
@@ -31,11 +27,9 @@ builder.Services.AddIdentity<Teacher, IdentityRole>(options =>
 }).AddEntityFrameworkStores<ApplicationDbContext>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -46,7 +40,6 @@ app.UseAuthentication();
 app.UseRouting();
 
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
